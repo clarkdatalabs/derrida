62a41f9e23e1ce5b3955256b64627254f7b51848
Hope Tambala <hopetambala@outlook.com>
2018-11-01 16:44:18 -0400

    chore(yaxis): turn into function

--- views/scripts/yAxis.js
+++ views/scripts/yAxis.js
@@ -6,30 +6,33 @@ var chartData = []
 
 
 //Parses Through Combined.csv in /data folder
-d3.csv("data/combined.csv", function(data) {
-    //console.log(data); //this would print out all the data in a list of objects from the CSV
-    //console.log(data[0]); //this would print out the first object in the list of objecs from the CSV
-    
-    /*
-        This loops through the list, retrieves that year published, 
-        converts it from a string into a number, removes NaN, sorts the keys ascendingly
-        and pushes those values into the chartData array
-    */
-    for(i=0;i<=data.length;i++){
-        if(data[i].Date !='NA'){
-            date = (Number(data[i].Date))
-            chartData.push(date)
-            chartData.sort(function(a, b){return a - b})
+function getData(){
+    parsedData = [];
+    d3.csv("data/combined.csv", function(data) {
+        //console.log(data); //this would print out all the data in a list of objects from the CSV
+        //console.log(data[0]); //this would print out the first object in the list of objecs from the CSV
+        /*
+            This loops through the list, retrieves that year published, 
+            converts it from a string into a number, removes NaN, sorts the keys ascendingly
+            and pushes those values into the chartData array
+        */
+        for(i=0;i<=data.length;i++){
+            if(data[i].Date !='NA'){
+                date = (Number(data[i].Date))
+                parsedData.push(date)
+                parsedData.sort(function(a, b){return a - b})
+            }
         }
-    } 
-    
-});
+        
+        
+    });
+    return parsedData
+}
 
+chartData = getData()
 //console.log(chartData) //This prints out new Chart Data
 //Filter Through Duplicates
-console.log((chartData))
-const values = Object.values(chartData)
-//console.log(values)
+
 
 
 var svg = d3.select("ul")
