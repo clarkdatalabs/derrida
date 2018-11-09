29c58e6bd2bb71268cc870bfbfd0471bb1f5360c
Hope Tambala <hopetambala@outlook.com>
2018-11-09 16:37:09 -0500

    feat(): tooltips

--- views/scripts/scatterChart.js
+++ views/scripts/scatterChart.js
@@ -24,26 +24,27 @@ d3.csv("data/combined.csv", function(data) {
 
     });
 
-  
+    //Margins
     var margin = {top: 20, right: 15, bottom: 60, left: 80}
     var width = 960 - margin.left - margin.right
     var height = 500 - margin.top - margin.bottom;
 
-    
+    //Number of Pages
     var minX = 0;
     var maxX = 445;
 
+    //Does Nothing
     var minY = d3.min(data, function(d) { return d.Date });
     var maxY = d3.max(data, function(d) { return d.Date });
     
     // Scale the range of the data  
     var x = d3.scaleLinear()
-              .domain([0, maxX])
+              .domain([minX, maxX])
               .range([ 0, width ]);
     
     var y = d3.scaleLinear()
               //.domain([0, 2017]) //There's some values assigned to 0 from data
-              .domain([1500, 2017])
+              .domain([1500, 2017]) //Years
     	      .range([ height, 0 ]);
  
     // Create Canvass
@@ -90,11 +91,31 @@ d3.csv("data/combined.csv", function(data) {
     
     var g = main.append("svg:g"); 
     
+    // Define the div for the tooltip
+    var tooltip = d3.select("body")
+        .append("div")   
+        .attr("class", "tooltip")               
+        .style("opacity", 0);
+
     // Add the scatterplot
     g.selectAll("scatter-dots")
       .data(data)
       .enter().append("circle")
           .attr("cx", function (d) { return x(d.page); } )
           .attr("cy", function (d) { return y(d.Date); } )
-          .attr("r", 4);
+          .attr("r", 4)
+          .on("mouseover", function(d) {
+            div.transition()     
+                .duration(200)      
+                .style("opacity", .9);      
+            div.html(d.Date)  
+                .style("left", (d3.event.pageX) + "px")     
+                .style("top", (d3.event.pageY - 28) + "px");    
+            })
+        .on("mouseout", function(d) {     
+            div.transition()        
+                .duration(500)      
+                .style("opacity", 0);   
+        });
+          
 });
